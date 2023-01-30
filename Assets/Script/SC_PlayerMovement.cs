08474441f9d35ef92528c1b5576cddd7c3dabd6e
Quentin144p <q.pavie@etudiant.ecolemelies.fr>
2023-01-30 20:09:47 +0100

    [QPA] add 2 script
    Add Character movement script & grabcompenent script

--- Assets/Script/SC_PlayerMovement.cs
+++ Assets/Script/SC_PlayerMovement.cs
@@ -1,16 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class SC_PlayerMovement : MonoBehaviour
 {
-    void Start()
+    private Rigidbody2D rb;
+    private float moveInput;
+    public float speed;
+    public float jumpForce;
+
+    private bool isGrounded;
+    public Transform feetPos;
+    public float checkRadius;
+    public LayerMask whatIsGround;
+
+    private void Awake()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    void Update()
+    private void Update()
     {
-        
+        isGrounded = Physics2D.OverlapCircle(feetPos.position, checkRadius, whatIsGround);
+    }
+
+    public void Move(InputAction.CallbackContext ctx)
+    {
+        moveInput = ctx.ReadValue<Vector2>().x;
+        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+    }
+
+    public void Jump(InputAction.CallbackContext ctx)
+    {
+        if(ctx.performed && isGrounded == true)
+        {
+            rb.velocity = new Vector2(moveInput * speed, jumpForce);
+            isGrounded = false;
+        }
     }
 }

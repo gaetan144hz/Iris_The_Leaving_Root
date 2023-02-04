430d972639eb0fc9d2804f0bb5009ddd9a865080
Gaétan <g.dumas@etudiant.ecolemelies.fr>
2023-02-04 17:41:27 +0100

    add Asticot&FixBug

--- Assets/Script/SC_GrabCompenent.cs
+++ Assets/Script/SC_GrabCompenent.cs
@@ -11,9 +11,18 @@ public class SC_GrabCompenent : MonoBehaviour
     [SerializeField] private Sprite newRockSprite;
     [SerializeField] private Sprite oldRockSprite;
     public Color flowerColor;
+    private Rigidbody2D rbRock;
+
+    private void Start()
+    {
+        rbRock = this.GetComponent<Rigidbody2D>();
+
+        rbRock.bodyType = RigidbodyType2D.Kinematic;
+    }
 
     public void Grab(Rigidbody2D rb)
     {
+        rbRock.bodyType = RigidbodyType2D.Dynamic;
         rockSprite.sprite = newRockSprite;
         joint.connectedBody = rb;
         joint.enabled = true;
@@ -23,6 +32,8 @@ public class SC_GrabCompenent : MonoBehaviour
     {
         if(joint.connectedBody != null)
         {
+            rbRock.velocity = Vector2.zero;
+            rbRock.bodyType = RigidbodyType2D.Kinematic;
             rockSprite.sprite = oldRockSprite;
             joint.connectedBody = null;
             joint.enabled = false;
